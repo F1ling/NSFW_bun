c34591e82d327a9cb0a0bbb0e09a239a700f46b1
F1lin <159540390+F1ling@users.noreply.github.com>
2025-07-05 20:42:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 from nudenet import NudeDetector  
 
 
@@ -10,12 +10,17 @@ bun_NSFW = {'class': True, 'score': True}
 
 @app.post("/moderate")
 async def moder_pic(upload_f: UploadFile):
-    filename = upload_f.filename
-    scan_pic = detector.detect(filename)
-    check_NSFW = [{
-    "class": t["class"] in scan_pic,
-    "score": t["score"]>0.7} for t in scan_pic]
-    if bun_NSFW in check_NSFW:
-        return {"BUN": "NSFW"}
-    return {"ok": "Safe"}
+    if upload_f.content_type not in ["image/jpeg", "image/png"]:
+        raise HTTPException(400, detail="Invalid file format. Only JPEG/PNG allowed")
+    try:
+        filename = upload_f.filename
+        scan_pic = detector.detect(filename)
+        check_NSFW = [{
+        "class": t["class"] in scan_pic,
+        "score": t["score"]>0.7} for t in scan_pic]
+        if bun_NSFW in check_NSFW:
+            return {"BUN": "NSFW"}
+        return {"ok": "Safe"}
+    except Exception as e:
+        raise HTTPException(500, detail=f"Processing error: {str(e)}")
  
